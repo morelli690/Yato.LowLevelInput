e616ee6232f22a2724cf12d694a2d8207a49d4a8
Yato <michel.pioch@gmx.de>
2017-12-04 04:23:52 +0100

    removed the example

--- Yato.Input/Program.cs
+++ Yato.Input/Program.cs
@@ -10,46 +10,7 @@ namespace Yato.Input
     {
         static void Main(string[] args)
         {
-            //using (var hook = new LowLevelKeyboardHook())
-            //{
-            //    hook.InstallHook();
-
-            //    hook.OnKeyCaptured += Hook_OnKeyCaptured;
-
-            //    Console.ReadLine();
-            //}
-
-            //using (var hook = new LowLevelMouseHook())
-            //{
-            //    hook.InstallHook();
-
-            //    hook.OnMouseCaptured += Hook_OnMouseCaptured;
-
-            //    Console.ReadLine();
-            //}
-
-            var input = new InputHandler(true);
-
-            var keyPressed = input.RegisterNamedEvent(VirtualKeyCode.A);
-
-            while(true)
-            {
-                if (!input.WaitForNamedEvent(keyPressed)) continue;
-
-                break;
-
-                //var key = input.WaitForNextEvent();
-
-                //Console.WriteLine(key);
-            }
-
-            Console.WriteLine("A");
-
-            input.UnregisterNamedEvent(keyPressed);
-
             Console.ReadLine();
-
-            input.Dispose();
         }
 
         private static void Hook_OnMouseCaptured(KeyState state, VirtualKeyCode key, int x, int y)
