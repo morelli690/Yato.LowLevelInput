96834db5d0932012ed2a6f6ffe7e054cd6d0e21e
Yato <michel.pioch@gmx.de>
2017-12-20 05:57:12 +0100

    fixed keycode alignment in converter

--- Yato.Input/Program.cs
+++ Yato.Input/Program.cs
@@ -11,12 +11,60 @@ namespace Yato.Input
     {
         static void Main(string[] args)
         {
-            using (var input = new InputHandler(true))
-            {
-                input.OnInputCaptured += Input_OnInputCaptured;
+            Console.WriteLine(KeyCodeConverter.ToString(VirtualKeyCode.SPACE));
 
-                Console.ReadLine();
+            Console.ReadLine();
+
+            //printKeyCodeArray();
+
+            //using (var input = new InputHandler(true))
+            //{
+            //    input.OnInputCaptured += Input_OnInputCaptured;
+
+            //    Console.ReadLine();
+            //}
+        }
+
+        private static void printKeyCodeArray()
+        {
+            List<string> nameList = new List<string>();
+
+            foreach (var value in Enum.GetValues(typeof(VirtualKeyCode)))
+            {
+                string name = Enum.GetName(typeof(VirtualKeyCode), value);
+
+                int index = (int)value;
+
+                if (index == nameList.Count)
+                {
+                    nameList.Add(name);
+                }
+                else
+                {
+                    int delta = index - nameList.Count;
+
+                    for (int i = 0; i < delta; i++)
+                        nameList.Add("");
+
+                    nameList.Add(name);
+                }
             }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("private static string[] strArray = new string[]");
+            sb.AppendLine("{");
+
+            foreach (var name in nameList)
+            {
+                sb.AppendLine("\"" + name + "\",");
+            }
+
+            sb.AppendLine("};");
+
+            Console.WriteLine(sb.ToString());
+
+            Console.ReadLine();
         }
 
         private static void Input_OnInputCaptured(KeyState state, VirtualKeyCode key, int x, int y)
