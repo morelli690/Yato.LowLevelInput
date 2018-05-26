98b80bb27324931c6fb0a87b190c73ad69179d4f
Yato <michel.pioch@gmx.de>
2018-05-26 21:43:21 +0200

    removed unused code

--- Yato.LowLevelInput/Program.cs
+++ Yato.LowLevelInput/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#if DEBUG
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,39 +9,10 @@ using System.Runtime.InteropServices;
 
 namespace Yato.LowLevelInput
 {
-    internal class Program
+    public class Program
     {
-        private static void Hook_OnKeyCaptured(KeyState state, VirtualKeyCode key)
+        public static void Main(string[] args)
         {
-            Console.WriteLine(state.ToString() + "\t:\t" + key.ToString());
-        }
-
-        private static void Hook_OnMouseCaptured(KeyState state, VirtualKeyCode key, int x, int y)
-        {
-            Console.WriteLine(state + "\t:\t" + key + "\tX: " + x + " Y: " + y);
-        }
-
-        private static void Input_OnInputCaptured(KeyState state, VirtualKeyCode key, int x, int y)
-        {
-            Console.WriteLine(state + "\t:\t" + key + "\tX: " + x + " Y: " + y);
-        }
-
-        private static void Main(string[] args)
-        {
-            //printItemDictionary();
-
-            //Console.WriteLine(KeyCodeConverter.ToString(VirtualKeyCode.SPACE));
-
-            //Console.ReadLine();
-
-            //printKeyCodeArray();
-
-            using (var input = new InputHandler(false))
-            {
-                input.OnInputCaptured += Input_OnInputCaptured;
-
-                Console.ReadLine();
-            }
         }
 
         private static void printKeyCodeArray()
@@ -85,3 +58,5 @@ namespace Yato.LowLevelInput
         }
     }
 }
+
+#endif
