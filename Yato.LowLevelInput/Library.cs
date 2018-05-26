e37497e04ef3ab2b5219507f5e0041cad7a455d8
Yato <michel.pioch@gmx.de>
2018-05-26 21:38:36 +0200

    Added Library info

---
+++ Yato.LowLevelInput/Library.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace Yato.LowLevelInput
+{
+    public static class Library
+    {
+        public static string Author
+        {
+            get
+            {
+                return "Yato";
+            }
+        }
+
+        public static string Name
+        {
+            get
+            {
+                return "Yato.LowLevelInput";
+            }
+        }
+
+        public static string URL
+        {
+            get
+            {
+                return "https://github.com/YatoDev/Yato.LowLevelInput";
+            }
+        }
+
+        public static string Version
+        {
+            get
+            {
+                try
+                {
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    AssemblyName assemblyName = assembly.GetName();
+
+                    return assemblyName.Version.ToString();
+                }
+                catch
+                {
+                    return "1.0.0.0";
+                }
+            }
+        }
+    }
+}
