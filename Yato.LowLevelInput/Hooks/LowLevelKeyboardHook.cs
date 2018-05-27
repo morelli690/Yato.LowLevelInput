b0be98b650324f5e7e0f2659598a7722110728aa
Yato <michel.pioch@gmx.de>
2018-05-27 18:21:53 +0200

    Implemented LowLevelKeyboardHook

--- Yato.LowLevelInput/Hooks/LowLevelKeyboardHook.cs
+++ Yato.LowLevelInput/Hooks/LowLevelKeyboardHook.cs
@@ -1,12 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+using Yato.LowLevelInput.PInvoke;
+using Yato.LowLevelInput.WindowsHooks;
 
 namespace Yato.LowLevelInput.Hooks
 {
-    class LowLevelKeyboardHook
+    public class LowLevelKeyboardHook : IDisposable
     {
+        private WindowsHook hook;
+        private object lockObject;
+
+        public LowLevelKeyboardHook()
+        {
+            lockObject = new object();
+        }
+
+        ~LowLevelKeyboardHook()
+        {
+            Dispose(false);
+        }
+
+        public delegate void KeyboardEventCallback(KeyState state, VirtualKeyCode key);
+
+        public event KeyboardEventCallback OnKeyboardEvent;
+
+        private void Hook_OnHookCalled(IntPtr wParam, IntPtr lParam)
+        {
+            if (lParam == IntPtr.Zero) return;
+            if (OnKeyboardEvent == null) return;
+
+            WindowsMessage msg = (WindowsMessage)((uint)wParam.ToInt32());
+
+            VirtualKeyCode key = (VirtualKeyCode)Marshal.ReadInt32(lParam);
+
+            switch (msg)
+            {
+                case WindowsMessage.WM_KEYDOWN:
+                    OnKeyboardEvent?.Invoke(KeyState.Down, key);
+                    break;
+
+                case WindowsMessage.WM_KEYUP:
+                    OnKeyboardEvent?.Invoke(KeyState.Up, key);
+                    break;
+
+                case WindowsMessage.WM_SYSKEYDOWN:
+                    OnKeyboardEvent?.Invoke(KeyState.Down, key);
+                    break;
+
+                case WindowsMessage.WM_SYSKEYUP:
+                    OnKeyboardEvent?.Invoke(KeyState.Up, key);
+                    break;
+            }
+        }
+
+        public bool InstallHook()
+        {
+            lock (lockObject)
+            {
+                if (hook != null) return false;
+
+                hook = new WindowsHook(WindowsHookType.LowLevelKeyboard);
+
+                hook.OnHookCalled += Hook_OnHookCalled;
+
+                hook.InstallHook();
+
+                return true;
+            }
+        }
+
+        public bool UninstallHook()
+        {
+            lock (lockObject)
+            {
+                if (hook == null) return false;
+
+                hook.OnHookCalled -= Hook_OnHookCalled;
+
+                hook.UninstallHook();
+
+                hook.Dispose();
+
+                hook = null;
+
+                return true;
+            }
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                }
+
+                UninstallHook();
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable Support
     }
 }
