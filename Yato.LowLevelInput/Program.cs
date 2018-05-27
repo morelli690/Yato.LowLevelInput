77bee8e0cddaf0dfb786272c5d28d63ba2f8366e
Yato <michel.pioch@gmx.de>
2018-05-27 18:19:57 +0200

    removed testing code

--- Yato.LowLevelInput/Program.cs
+++ Yato.LowLevelInput/Program.cs
@@ -14,48 +14,6 @@ namespace Yato.LowLevelInput
         public static void Main(string[] args)
         {
         }
-
-        private static void PrintKeyCodeArray()
-        {
-            List<string> nameList = new List<string>();
-
-            foreach (var value in Enum.GetValues(typeof(VirtualKeyCode)))
-            {
-                string name = Enum.GetName(typeof(VirtualKeyCode), value);
-
-                int index = (int)value;
-
-                if (index == nameList.Count)
-                {
-                    nameList.Add(name);
-                }
-                else
-                {
-                    int delta = index - nameList.Count;
-
-                    for (int i = 0; i < delta; i++)
-                        nameList.Add("");
-
-                    nameList.Add(name);
-                }
-            }
-
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine("private static string[] strArray = new string[]");
-            sb.AppendLine("{");
-
-            foreach (var name in nameList)
-            {
-                sb.AppendLine("\"" + name + "\",");
-            }
-
-            sb.AppendLine("};");
-
-            Console.WriteLine(sb.ToString());
-
-            Console.ReadLine();
-        }
     }
 }
 
