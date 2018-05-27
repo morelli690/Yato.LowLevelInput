8a623cf0761f9ffc1d2afdb9a241e90ea6c64c96
Yato <michel.pioch@gmx.de>
2018-05-28 00:38:35 +0200

    added a placeholder here

--- Yato.LowLevelInput/WindowsHooks/WindowsHookType.cs
+++ Yato.LowLevelInput/WindowsHooks/WindowsHookType.cs
@@ -13,6 +13,7 @@ namespace Yato.LowLevelInput.WindowsHooks
         CBT = 5,
         SysMsgFilter = 6,
         Mouse = 7,
+        Undocumented = 8,
         Debug = 9,
         Shell = 10,
         ForegroundIdle = 11,
