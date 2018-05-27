1ea0ea4b91b4cb59afbeab505742514f36d84a7f
Yato <michel.pioch@gmx.de>
2018-05-27 20:12:52 +0200

    added CreateThread import

--- Yato.LowLevelInput/PInvoke/Kernel32.cs
+++ Yato.LowLevelInput/PInvoke/Kernel32.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Yato.LowLevelInput.PInvoke
 {
     internal static class Kernel32
     {
+        public static CreateThread_t CreateThread = WinApi.GetMethod<CreateThread_t>("kernel32.dll", "CreateThread");
         public static GetCurrentThreadId_t GetCurrentThreadId = WinApi.GetMethod<GetCurrentThreadId_t>("kernel32.dll", "GetCurrentThreadId");
+
+        public delegate IntPtr CreateThread_t(IntPtr lpThreadAttributes, IntPtr dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, ref uint lpThreadId);
 
         public delegate uint GetCurrentThreadId_t();
     }
