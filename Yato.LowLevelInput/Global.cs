6c8690e9792deece0a21e27d3623ba4adb45f15c
Yato <michel.pioch@gmx.de>
2018-05-28 00:52:19 +0200

    Added Global - Removed ProcessEvents

---
+++ Yato.LowLevelInput/Global.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Yato.LowLevelInput
+{
+    internal static class Global
+    {
+        static Global()
+        {
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        public delegate void ProcessExitCallback();
+
+        public delegate void UnhandledExceptionCallback();
+
+        public static event ProcessExitCallback OnProcessExit;
+
+        public static event UnhandledExceptionCallback OnUnhandledException;
+
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            OnProcessExit?.Invoke();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            OnUnhandledException?.Invoke();
+        }
+    }
+}
