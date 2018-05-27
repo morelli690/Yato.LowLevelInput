54805c660a8eb22cceec694b836e7fb108ddee8a
Yato <michel.pioch@gmx.de>
2018-05-27 20:13:01 +0200

    added NtSetInformationThread

---
+++ Yato.LowLevelInput/PInvoke/NtDll.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Yato.LowLevelInput.PInvoke
+{
+    internal static class NtDll
+    {
+        public static NtSetInformationThread_t NtSetInformationThread = WinApi.GetMethod<NtSetInformationThread_t>("ntdll.dll", "NtSetInformationThread");
+
+        public delegate int NtSetInformationThread_t(IntPtr hThread, int threadInformationClass, IntPtr threadInformation, int threadInformationLength);
+    }
+}
